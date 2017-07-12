59950046c58a0614803a762d1b9b7972b9b1ce49
Eduard Urbach <e.urbach@gmail.com>
2017-07-12 22:00:39 +0200

    Added http server

---
+++ server/server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/animenotifier/japanese"
+)
+
+func japaneseTokenizer(w http.ResponseWriter, req *http.Request) {
+	data := japanese.Tokenize(strings.TrimPrefix(req.URL.Path, "/"))
+	buffer, err := json.Marshal(data)
+
+	if err != nil {
+		io.WriteString(w, err.Error())
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Write(buffer)
+}
+
+func main() {
+	http.HandleFunc("/", japaneseTokenizer)
+	log.Fatal(http.ListenAndServe(":1337", nil))
+}
